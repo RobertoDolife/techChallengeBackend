d9c1bcd310e940b08c961e0e8fd90821c0680ddc
Thatygomes <thatygomes@gmail.com>
2025-07-24 20:21:49 -0300

    adiciona configuração do banco de dados com Sequelize e variáveis de ambiente

---
+++ src/database/sequelize.js
@@ -0,0 +1,5 @@
+const { Sequelize } = require('sequelize')
+
+const { env } = require('../common/env')
+
+exports.sequelize = new Sequelize(`mysql://${env.DATABASE_USERNAME}:${env.DATABASE_PASSWORD}@${env.DATABASE_HOST}:${env.DATABASE_PORT}/SchoolOnDb`)
