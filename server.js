5ecd4a3a73c52bff7924f783f9602909e4bc9f29
Thatygomes <thatygomes@gmail.com>
2025-08-03 03:19:13 -0300

    fix(server): update database connection references to use sequelize directly

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const app = require('./src/app');
-const db = require('./src/models');
+const { sequelize } = require('./src/database/sequelize')
 const {env} = require('./src/common/env')
 
-db.sequelize.authenticate()
+sequelize.authenticate()
   .then(() => {
     console.log('Conectado ao banco com sucesso!');
-    return db.sequelize.sync({ alter: true });
+    return sequelize.sync({ alter: true });
   })
   .then(() => {
     console.log('🛠️ Modelos sincronizados com o banco');
