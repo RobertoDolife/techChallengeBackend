0a79b382745e59e350e9402562409d24d422da30
Thatygomes <thatygomes@gmail.com>
2025-08-03 03:14:44 -0300

    fix(sequelize): disable logging for Sequelize instance

--- src/database/sequelize.js
+++ src/database/sequelize.js
@@ -2,4 +2,4 @@ const { Sequelize } = require('sequelize')
 
 const { env } = require('../common/env')
 
-exports.sequelize = new Sequelize(`mysql://${env.DATABASE_USERNAME}:${env.DATABASE_PASSWORD}@${env.DATABASE_HOST}:${env.DATABASE_PORT}/SchoolOnDb`)
+exports.sequelize = new Sequelize(`mysql://${env.DATABASE_USERNAME}:${env.DATABASE_PASSWORD}@${env.DATABASE_HOST}:${env.DATABASE_PORT}/SchoolOnDb`, { logging: false })
