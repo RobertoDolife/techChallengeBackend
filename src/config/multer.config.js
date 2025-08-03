1ca512314b88660faaa6003cbf9ee99fb8a317bc
Thatygomes <thatygomes@gmail.com>
2025-08-03 03:14:18 -0300

    fix(multer): update upload destination path to 'uploads' directory

--- src/config/multer.config.js
+++ src/config/multer.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '..', '..', 'public', 'uploads', 'posts'));
+    cb(null, path.join(__dirname, '..', '..', 'uploads'));
   },
   filename: (req, file, cb) => {
     const uniqueName = `${Date.now()}-${file.originalname}`;
